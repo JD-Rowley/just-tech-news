f57ae3863870af5092f7a5ea4512ecb2befc1e40
JD Rowley <jdrowley1989@gmail.com>
2021-10-19 12:39:34 -0600

    optimize/refactor code, add pluralization to words

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const path = require('path');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const sess = {
     secret: 'Super secret secret',
@@ -19,6 +19,7 @@ const sess = {
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
